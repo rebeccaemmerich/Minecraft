6e4fffb34e90b4cdbe6ee1f015ab81ddd6c05b80
nmrosenthal <nmrosenthal@gmail.com>
2017-01-04 13:23:49 +0200

    basic layout

--- Minecraft.js
+++ Minecraft.js
@@ -1,9 +1,32 @@
+var minecraft = {};
+
+
+
+minecraft.initBoard = function(){ 
+
 var gameboard = $("#gameboard");
+var toolbox = $("#toolbox");
 
 
-for (var v = 0; v < 10; v++) {
-
-    for (var w = 0; w < 10; w++) {
-        $('#gameboard').append($("<div></div>").addClass("pixel"));
+//create matrix
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+        gameboard = $('#gameboard');
+        gameboard.append($("<div></div>").addClass("pixel").data("i",i).data("j",j));
     }
 }
+
+//assign classes to each pixel
+$(".pixel").each(function(){
+	if($(this).data("i")>6){
+		$(this).addClass("earth");
+		}
+	if($(this).data("i")===6){
+		$(this).addClass("surface");
+	} else {
+		$(this).addClass("sky");
+	}
+	});
+}
+
+minecraft.initBoard();
