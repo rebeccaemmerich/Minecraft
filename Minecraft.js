521ed3268e94c4461888a7ac3c43d56963f0b49f
rebeccaemmerich <rebeccaemmerich@gmail.com>
2017-01-04 11:17:08 +0200

    grid

--- Minecraft.js
+++ Minecraft.js
@@ -1,1 +1,9 @@
+var gameboard = $("#gameboard");
 
+
+for (var v = 0; v < 10; v++) {
+
+    for (var w = 0; w < 10; w++) {
+        $('#gameboard').append($("<div></div>").addClass("pixel"));
+    }
+}
